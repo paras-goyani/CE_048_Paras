c67d075ae55a2314e7109e14c6f6753d0ba2071d
paras-goyani <68722546+paras-goyani@users.noreply.github.com>
2020-08-01 14:52:31 +0530

    Create helloworld.cs

---
+++ LAB_1/helloworld.cs
@@ -0,0 +1,12 @@
+using System;
+namespace driver_prog
+{
+	public class my_driver_class
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("hello world");
+			Console.ReadKey();
+		}
+	}
+}
