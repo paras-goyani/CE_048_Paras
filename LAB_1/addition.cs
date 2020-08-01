2951fcf4a4ec5a8280e200c9f8bb105a73ccadd6
paras-goyani <68722546+paras-goyani@users.noreply.github.com>
2020-08-01 14:56:08 +0530

    Add files via upload

---
+++ LAB_1/addition.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace lab1_addition
+{
+    public class lab1_addition_class
+    {
+        public int addition(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ LAB_1/driver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using lab1_addition;
+namespace driver_prog
+{
+	public class my_driver_class
+	{
+		static void Main(string[] args)
+		{
+			lab1_addition_class c = new lab1_addition_class();
+			int ans = c.addition(2, 3);
+			Console.WriteLine(ans);
+			Console.ReadKey();
+		}
+	}
+}
