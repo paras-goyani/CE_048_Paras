38939a633d6a1952e69264482b9c778929352bed
paras-goyani <68722546+paras-goyani@users.noreply.github.com>
2020-08-01 14:55:18 +0530

    Add files via upload

---
+++ LAB_1/addition_user_input.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace driver_prog
+{
+	public class my_driver_class
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter number1 = ");
+			double number1=double.Parse(Console.ReadLine());
+			
+			Console.WriteLine("\nEnter number2 = ");
+			double number2=double.Parse(Console.ReadLine());
+			double sum=number1+number2;
+			
+			Console.WriteLine("sum = "+sum);
+			//Console.ReadKey();
+		}
+	}
+}
