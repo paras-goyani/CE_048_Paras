eadc843db26153269badc2fd698709a3d6db3f18
paras-goyani <68722546+paras-goyani@users.noreply.github.com>
2020-08-09 17:12:13 +0530

    before LAB_2 and LAB_4 name exchanged

---
+++ LAB_2/addition.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace addition
+{
+    public class addition_class
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+    }
+	
+	
+	
+	
+}
---
+++ LAB_2/multiplication.cs
@@ -0,0 +1,15 @@
+using System;
+namespace multiplication
+{
+    
+	
+	public class multiplication_class
+    {
+        public int mul(int x, int y)
+        {
+            return x*y;
+        }
+    }
+	
+	
+}
